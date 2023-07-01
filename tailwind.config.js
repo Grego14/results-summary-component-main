145d961f742120f7432340a169bbe7951ef39ada
Gregorio <gre208981@gmail.com>
2023-07-01 01:14:20 -0400

    updated fonts links #3

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
   theme: {
     extend: {},
     fontFamily: {
-      HGBold: ["HankenGrotesk-Bold", "sans-serif"],
       HGMedium: ["HankenGrotesk-Medium", "sans-serif"],
+      HGBold: ["HankenGrotesk-Bold", "sans-serif"],
       HGExtraBold: ["HankenGrotesk-ExtraBold", "sans-serif"],
     },
     colors: {
@@ -17,10 +17,10 @@ module.exports = {
       "orangey-yellow": "hsl(39, 100%, 56%)",
       "green-teal": "hsl(166, 100%, 37%)",
       "cobalt-blue": "hsl(234, 85%, 45%)",
-      "light-red-o": "hsl(0, 100%, 67%, .1)",
-      "orangey-yellow-o": "hsl(39, 100%, 56%, .1)",
-      "green-teal-o": "hsl(166, 100%, 37%, .1)",
-      "cobalt-blue-o": "hsl(234, 85%, 45%, .1)",
+      "light-red-o": "hsl(0, 100%, 67%, .05)",
+      "orangey-yellow-o": "hsl(39, 100%, 56%, .05)",
+      "green-teal-o": "hsl(166, 100%, 37%, .05)",
+      "cobalt-blue-o": "hsl(234, 85%, 45%, .05)",
       "dark-gray-blue-idk": "hsl(224, 30%, 27%, .5)",
       "pale-blue": "hsl(221, 100%, 96%)",
       "light-lavender": "hsl(241, 100%, 89%)",
@@ -32,8 +32,8 @@ module.exports = {
     },
     screens: {
       mobile: { min: "320px", max: "576px" },
-      "mobile-landscape": { raw: "(min-height: 320px) and (orientation: landscape)" },
-      "mobile-portrait": { raw: "(max-height: 800px) and (orientation: portrait)" },
+      "mobile-landscape": { raw: "(max-height: 550px) and (orientation: landscape)" },
+      "mobile-portrait": { raw: "(max-height: 850px) and (orientation: portrait)" },
       tablet: "768px",
       laptop: "992px",
       desktop: "1200px",
